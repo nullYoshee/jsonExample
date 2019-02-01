2a63630d1a7f6c6e6af9af99fd595cd0628cc9f1
nullYoshee <41447621+nullYoshee@users.noreply.github.com>
2019-02-01 09:13:55 -0600

    new images

---
+++ petsOnly.js
@@ -0,0 +1,32 @@
+{
+  "pets": [
+    {
+      "name" : "Butch Catsidy",
+      "species" : "Cat",
+      "favFoods" : ["wet food", "dry food", "any food"],
+      "birthYear" : 2016,
+      "photo" : "http://www.wallpapergeeks.com/wp-content/uploads/2015/04/mellow-cat-wallpaper-800x600.jpg"
+    },
+    {
+      "name" : "Seymour Butz",
+      "species" : "Dog",
+      "favFoods" : ["table scraps", "roast beef", "bacon"],
+      "birthYear" : 2016,
+      "photo" : "https://static1.squarespace.com/static/5720df69f699bb2aca11a932/t/572524811d07c02f9c23612c/1462051985425/?format=750w"
+    },
+    {
+      "name" : "Chewbarka",
+      "species" : "Dog",
+      "favFoods" : ["smooshy stuff", "own poop", "food that can kill me"],
+      "birthYear" : 2014,
+      "photo" : "http://doggiemanners.com/wp-content/uploads/2015/01/dog-at-computer-wp.jpg"
+    },
+    {
+      "name" : "Allie Cat",
+      "species" : "Cat",
+      "favFoods" : ["fresh tuna", "catnip", "rabbit"],
+      "birthYear" : 2012,
+      "photo" : "http://www.wallpapergeeks.com/wp-content/uploads/2013/11/lynx-wild-cat-wallpaper-800x600.jpg"
+    }
+  ]
+}
